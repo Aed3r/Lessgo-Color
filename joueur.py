e53f7602fc34d2b2dfe7273307f4e5bd21ec659c
Marwan aitaddi <marwanaitaddi@pop-os.localdomain>
2021-03-09 19:17:17 +0100

    fin joueur et fichier de test pour joueur

--- joueur.py
+++ joueur.py
@@ -1,11 +1,14 @@
 #Classe servant a instancier des joueurs
 class Joueur:
-    def __init__(self, id):
+    def __init__(self, id, x, y):
         self.id = id
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
+        self.dead = False
     def translation(self, dx, dy):
         self.x += dx
         self.y += dy
     def isDead(self):
         return self.dead
+    def setDead(self, valeur):
+        self.dead = valeur

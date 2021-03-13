494dd63b5b478e04b5c308e21615b807a43d7352
Woople74 <Darkmar74@outlook.com>
2021-03-13 17:58:55 +0100

    Affichage des joueurs

--- joueur.py
+++ joueur.py
@@ -1,10 +1,21 @@
 #Classe servant a instancier des joueurs
-class Joueur:
-    def __init__(self, id, x, y):
+***REMOVED***
+
+class Joueur(object):
+    def __init__(self, id, x, y, equipe):
         self.id = id
         self.x = x
         self.y = y
         self.dead = False
+        self.equipe = equipe
+        if equipe == 0:
+            self.color = (0, 127, 255)
+        elif equipe == 1:
+            self.color = (255, 255, 0)
+        elif equipe == 2:
+            self.color = (187, 11, 11)
+        elif equipe == 3:
+            self.color = (0,255,0) 
     def translation(self, dx, dy):
         self.x += dx
         self.y += dy
@@ -12,3 +23,9 @@ class Joueur:
         return self.dead
     def setDead(self, valeur):
         self.dead = valeur
+    def afficher(self, fenetre):
+        if !self.isDead():
+            pygame.draw.circle(fenetre, self.color, [self.x, self.y], 5)
+        else:
+            self.x = -1
+            self.y = -1

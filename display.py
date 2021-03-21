c3026b8b8c2f23139440beccd2ebbe7f691d9b95
os <os@os>
2021-03-21 22:14:15 +0100

    fusion affichage

--- display.py
+++ display.py
@@ -2,20 +2,73 @@
 import threading
 import time
 from joueur import *
+from plateau import *
 
-class affichage(threading.Thread):  
-    def __init__(self):  
-        threading.Thread.__init__(self)
-  
-    def run(self):  
-        t = threading.currentThread()
-        while getattr(t, "do_run", True):
-            # Affichage du plateau et des joueurs, s'arrête avec le serveur
-            # afficherJoueur(fenetre) 
-            pass
+                                                                                                                     
+***REMOVED***                                                                                                         #initialise les module pygame
+pygame.display.set_caption("SPLAT_PGMOT")                                                                             #nom fenetre
+info = pygame.display.Info()                                                                                          #recupere l'information de la machine en cours
+resolution = (info.current_w,info.current_h)                                                                          #resolution de l'ecrant a partir de info
+fenetre = pygame.display.set_mode(resolution,pygame.FULLSCREEN)                                                       #FULLSCREEN
+boucler = True                                                                                                        #Boolean boucle principale
+tailleCase = 20
+terrain = Terrain(200,400)
+
+#------------------------------------------------------------FONCTION--------------------------------------------------------------------------------------#
 
 def afficherJoueurs(fenetre):
     fenetre.fill((80,80,80)) #couleur fenetre
     joueurs.sort(key=comparJoueur) # Trie les joueurs suivant leurs ordonnées 
     for joueur in joueurs:
         joueur.afficher(fenetre)
+
+def fond():                                                                                                              #Fonction initialise fond du terrain
+    fenetre.fill((210,210,210))                                                                                           #couleur fond
+    pygame.draw.rect(fenetre,(100,0,0),pygame.Rect(0,0,200,200))                                                          #haut gauche rouge
+    pygame.draw.rect(fenetre,(0,100,0),pygame.Rect(info.current_w-200,0,200,200))                                         #haut droit vert
+    pygame.draw.rect(fenetre,(0,0,100),pygame.Rect(0,info.current_h-200,200,200))                                         #bas gauche bleu
+    pygame.draw.rect(fenetre,(100,100,0),pygame.Rect(info.current_w-200,info.current_h-200,200,200))                      #bas droit jaune
+
+def joueur(x,y):                                                                                                         #Fonction dessine un joueur en X,Y
+    pygame.draw.circle(fenetre,(0,0,0),[x, y], 5)                                                                         #joueur representer par un cercle
+
+def afficheTerrain(terrain):
+    for i in range(terrain.larg):
+            for j in range (terrain.long):
+                    if(terrain.getColor(i,j) == 1):
+                          pygame.draw.rect(fenetre,(255,255,255),pygame.Rect(i*tailleCase,j*tailleCase,tailleCase,tailleCase))    
+                    elif(terrain.getColor(i,j) == 2):
+                          pygame.draw.rect(fenetre,(100,000,0),pygame.Rect(i*tailleCase,j*tailleCase,tailleCase,tailleCase))
+
+def initTerrain(terrain):
+     for i in range(terrain.larg):
+            for j in range (terrain.long):
+                if((i+j)%2 == 0):
+                    terrain.setColor(i,j,1)
+                else :
+                    terrain.setColor(i,j,2)
+
+
+class affichage(threading.Thread):  
+    def __init__(self):  
+        threading.Thread.__init__(self)
+        initTerrain(terrain)
+
+#------------------------------------------------------------BOUCLE PRINCIPALE-----------------------------------------------------------------------------#
+
+    def run(self):  
+        t = threading.currentThread()
+        while getattr(t, "do_run", True):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    boucler = False
+                if event.type == pygame.KEYDOWN:
+                    boucler = False
+            afficheTerrain(terrain)
+            # Affichage du plateau et des joueurs, s'arrête avec le serveur
+            afficherJoueur(fenetre)
+            pygame.display.flip()                                                                                               #actualise
+            pass
+        
+#----------------------------------------------------------------------------------------------------------------------------------------------------------#
+        

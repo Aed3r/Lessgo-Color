58d47f101b394292fffcb4b5f8058c7eba02ced6
Paul-Antoine Bernard <bernard.paul.antoine@gmail.com>
2021-03-26 16:00:20 +0100

    ajout constantes

--- constantes.py
+++ constantes.py
@@ -1,4 +1,10 @@
+# ----------------- Display.py ----------------- #
 resolution = (1080, 720)
-fps = 60
 playerSize = 5
+tailleCase = 20
+
+fps = 60
+msPerFrame = int(1000 / fps)
+
+# ------------------ Main.py ------------------- #
 port = 8081
